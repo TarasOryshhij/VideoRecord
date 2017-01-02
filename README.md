85b366b04a4decf591248f2efd36e898fd47954d
Taras Oryshchii <taras@pinta.com.ua>
2017-01-02 23:26:06 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Video record application
--- app/src/main/java/taras/com/ua/videorecord/MainActivity.java
+++ app/src/main/java/taras/com/ua/videorecord/MainActivity.java
@@ -102,4 +102,5 @@ public class MainActivity extends AppCompatActivity {
         public void onServiceDisconnected(ComponentName arg0) {
         }
     };
+
 }
